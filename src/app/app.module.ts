68d42e5e046a87835b797adffffa8fb5a0c82662
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-08-27 12:21:08 +0300

    generate pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { PostDetailPageComponent } from './pages/post-details-page/post-detail-page.component';
+import { SearchResultPageComponent } from './pages/search-result-page/search-result-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CategoriesPageComponent,
+    PostDetailPageComponent,
+    SearchResultPageComponent
   ],
   imports: [
     BrowserModule,
