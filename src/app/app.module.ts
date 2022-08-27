665ad85d896c9e97d1000836c5ee506c7e1f145e
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-08-27 15:00:34 +0300

    update search component & style import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { CategoryPageComponent } from './pages/categories-page/category-page.component';
-import { PostDetailPageComponent } from './pages/post-details-page/post-detail-page.component';
-import { SearchResultPageComponent } from './pages/search-result-page/search-result-page.component';
+import { CommonSearchComponent } from './components/common-search/common-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    CategoryPageComponent,
-    PostDetailPageComponent,
-    SearchResultPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule
-  ],
+  declarations: [AppComponent, CommonSearchComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomePageComponent } from '../pages/home-page/home-page.component';
+import { CategoryPageComponent } from '../pages/categories-page/category-page.component';
+import { PostDetailPageComponent } from '../pages/post-details-page/post-detail-page.component';
+import { SearchResultPageComponent } from '../pages/search-result-page/search-result-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HomePageComponent,
+    CategoryPageComponent,
+    PostDetailPageComponent,
+    SearchResultPageComponent,
+  ],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
